c18f9848e729f6bbb3b4acd1e6cfae949a7a9ae6
SixGangs <2577944135@qq.com>
2017-05-21 13:10:19 +0800

    提交所有

--- main/main.js
+++ main/main.js
@@ -1,3 +1,13 @@
-module.exports = function main(email, suffixes) {
-  // Write your code here
+//email地址后缀检测程序，判断给定的email程序是否在指定的集合中
+module.exports = function main(email, suffixes) 
+{
+  //取出email的后缀
+  var suffix = email.split("@")[1];
+  //遍历给定集合，查找该后缀是否存在于给定的集合中
+  for(var i = 0; i < suffixes.length; i++)
+  {
+	  if(suffixes[i] == suffix)
+		  return true;
+  }
+  return false;
 };
